ca59332eb8213fc021471adf9401180501e27948
Vishwasahukar <118972795+Vishwasahukar@users.noreply.github.com>
2022-12-12 20:33:55 +0530

    Update shell.sh

--- Jenkins/shell.sh
+++ Jenkins/shell.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 X=$(grep -c "Critical" $WORKSPACE/log.xls)
-num=3
+num=10
 if [ $X > $num ]; then
         echo "Vulnerablities are too high so not running further jobs"
 else
-        echo "wget -q --auth-no-challenge --user Vishwanatha --password Sahukar@33 http://3.125.124.144:8080/job/Devsecops-2/build?token=0987654321"
+        echo "wget -q --auth-no-challenge --user Vishwanatha --password Sahukar@33 http://3.125.124.144:8080/job/Devsecops-2/build?token=0987654321"        
 fi
