ab327dfdfd21f0557e0f27f828c4d97adbcaef67
Vishwasahukar <118972795+Vishwasahukar@users.noreply.github.com>
2022-12-12 20:25:17 +0530

    Update shell.sh

--- Jenkins/shell.sh
+++ Jenkins/shell.sh
@@ -4,5 +4,5 @@ num=3
 if [ $X > $num ]; then
         echo "Vulnerablities are too high so not running further jobs"
 else
-        echo "wget -q --auth-no-challenge --user Vishwanatha --password Sahukar@33 $http://3.125.124.144:8080/job/Devsecops-2/build?token=0987654321"
+        echo "wget -q --auth-no-challenge --user Vishwanatha --password Sahukar@33 http://3.125.124.144:8080/job/Devsecops-2/build?token=0987654321"
 fi
