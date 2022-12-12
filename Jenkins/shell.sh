9d969d7a3ef48cc48dc07973e66c238c56fb8b93
Vishwasahukar <118972795+Vishwasahukar@users.noreply.github.com>
2022-12-12 19:28:16 +0530

    Update shell.sh

--- Jenkins/shell.sh
+++ Jenkins/shell.sh
@@ -2,7 +2,7 @@
 X=$(grep -c "Critical" $WORKSPACE/log.xls)
 num=3
 if [ $X > $num ]; then
-        echo "curl -XPOST $http://3.125.124.144:8080/job/Devsecops-1/stop"
+        echo -e"sleep 1m"\n"curl -XPOST http://3.125.124.144:8080/job/Devsecops-1/stop"
 else
         echo "vulnerablities are low"
 fi
